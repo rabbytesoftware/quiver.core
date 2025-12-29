1261509d46679b688cacccd440a1c5923b0aa597
Agustin Gil <agustingil83@gmail.com>
2025-12-29 20:16:17 -0300

    Add arrow routes

--- internal/api/v1/routes/arrows/routes.go
+++ internal/api/v1/routes/arrows/routes.go
@@ -6,4 +6,25 @@ import (
 )
 
 func SetupRoutes(router *gin.RouterGroup, usecases *usecase.ArrowsUsecase) {
+	/*WEBSOCKETS (DO NOT MOVE GROUP)*/
+	
+	ws := router.Group("/ws")
+
+	// Listen arrow channels
+	ws.GET("/")
+
+	/*HTTP (DO NOT MOVE GROUP)*/
+
+	http := router.Group("/")
+
+	// Execute method
+	http.GET("/:namespace/execute/:method")
+	// Add arrow (by namespace or path)
+	http.POST("/:namespace")
+	// Get single arrow in library
+	http.GET("/:namespace")
+	// List arrows in library
+	http.GET("")
+	// Remove arrow
+	http.DELETE("/:namespace")
 }
