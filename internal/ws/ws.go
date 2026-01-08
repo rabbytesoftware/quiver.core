e99a7e4cf69b08fa113e442d06ff9e99aef4a749
JGGeist <joaquinggeist@gmail.com>
2026-01-08 20:06:02 -0300

    handler interface and Serve Loop

--- internal/ws/ws.go
+++ internal/ws/ws.go
@@ -22,3 +22,41 @@ type Dialer interface {
 type Upgrader interface {
 	Upgrade(ctx context.Context) (Conn, error)
 }
+
+type Handler interface {
+	OnOpen(ctx context.Context, c Conn) error
+
+	OnMessage(ctx context.Context, c Conn, data []byte, typ FrameType) error
+
+	OnError(ctx context.Context, c Conn, err error)
+
+	OnClose(ctx context.Context, c Conn, err error)
+}
+
+type HandlerFactory interface {
+	New(ctx context.Context, c Conn) Handler
+}
+
+func Serve(ctx context.Context, c Conn, h Handler) {
+	if err := h.OnOpen(ctx, c); err != nil {
+		_ = c.Close(1002, err.Error())
+		h.OnClose(ctx, c, err)
+		return
+	}
+
+	for {
+		data, typ, err := c.Read(ctx)
+		if err != nil {
+			h.OnError(ctx, c, err)
+			h.OnClose(ctx, c, err)
+			return
+		}
+
+		if err := h.OnMessage(ctx, c, data, typ); err != nil {
+			h.OnError(ctx, c, err)
+			_ = c.Close(1002, err.Error())
+			h.OnClose(ctx, c, err)
+			return
+		}
+	}
+}
