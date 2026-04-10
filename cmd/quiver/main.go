1f60c3d5cf711b8480aedf83d89abd007bc81340
Mateo Urrutia <me@char2cs.net>
2026-04-10 13:10:15 -0300

    feat(cmd): implement server startup with slog

--- cmd/quiver/main.go
+++ cmd/quiver/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/rabbytesoftware/quiver/internal"
+)
+
+func main() {
+	ctx := context.Background()
+
+	container, err := internal.Init(ctx)
+	if err != nil {
+		slog.Error("failed to initialize", "error", err)
+		os.Exit(1)
+	}
+
+	addr := ":8080"
+	slog.Info("starting quiver server", "addr", addr)
+	if err := container.API.Run(addr); err != nil {
+		slog.Error("server stopped", "error", err)
+		os.Exit(1)
+	}
+}
