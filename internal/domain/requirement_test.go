c419e4ba2a1c9cc20dbb507ea8e27f0a86bda495
Mateo Urrutia <me@char2cs.net>
2026-04-19 13:08:45 -0300

    refactor(domain): remove dead helper functions after testify migration

--- internal/domain/requirement_test.go
+++ internal/domain/requirement_test.go
@@ -97,12 +97,3 @@ func TestRequirement_Validate(t *testing.T) {
 		})
 	}
 }
-
-func contains(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
-}
--- internal/domain/variable_test.go
+++ internal/domain/variable_test.go
@@ -325,12 +325,3 @@ func TestVariable_Validate(t *testing.T) {
 		})
 	}
 }
-
-func containsStr(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
-}
